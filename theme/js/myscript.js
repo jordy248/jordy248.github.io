00f73e3c3c1bc67b409c54f24c1f073232ba5074
jordy248 <jordan.nelson248@gmail.com>
2019-03-19 09:09:38 -0400

    update theme

--- theme/js/myscript.js
+++ theme/js/myscript.js
@@ -1,3 +1,8 @@
-jQuery(document).ready(function() {
-//    jQuery('body').removeClass('loading');
+//jQuery(document).ready(function() {
+ // jQuery('body').removeClass('loading');
+//})
+
+jQuery(window).on("load", function() {
+    jQuery('body').removeClass('loading');
 })
+
