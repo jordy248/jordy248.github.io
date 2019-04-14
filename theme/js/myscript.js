98ac0ec4864929fdce2e9309ebf71ba7b0f0fb7a
jordy248 <jordan.nelson248@gmail.com>
2019-04-14 02:31:11 -0400

    update comments

--- theme/js/myscript.js
+++ theme/js/myscript.js
@@ -2,3 +2,7 @@ jQuery(window).on("load", function() {
     jQuery('body').removeClass('loading');
 })
 
+jQuery('#show-disqus').on('click', function(){
+    jQuery(this).parent().addClass('activated');
+})
+
